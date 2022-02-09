2da75a951eb0c190a44105de227ef71d60f175cd
Minimanx <Johanschwennesen@gmail.com>
2022-02-09 03:15:18 +0100

    start of movie api

---
+++ 1.1_Movies_API/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const listOfMovies = [];
+let id = 1;
+
+app.get("/movies", (req, res) => {
+    res.send(listOfMovies);
+});
+
+app.get("/movies/:id", (req, res) => {
+    const getById = parseInt(req.params.id);
+    const movie = listOfMovies.find(n => n.id === getById);
+    res.send(movie);
+});
+
+app.post("/movies", (req, res) => {
+    const movie = req.body;
+    movie.id = id;
+    id++;
+    listOfMovies.push(movie);
+    res.send(movie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+    const getById = parseInt(req.params.id);
+    const movieIndex = listOfMovies.findIndex(x => x.id === getById);
+    if(movieIndex === -1){
+        res.send({message: `${getById} not found`});
+    }
+    else{
+        const movie = listOfMovies.splice(movieIndex, 1);
+        res.send(movie);
+    }
+});
+
+
+
+
+
+
+
+
+
+app.listen(8080);

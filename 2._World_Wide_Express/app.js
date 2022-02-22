c65bd4658a42ae00fa7a6ccd9556f1129b9037df
Minimanx <Johanschwennesen@gmail.com>
2022-02-22 10:38:47 +0100

    frontpage folder

--- 2._World_Wide_Express/app.js
+++ 2._World_Wide_Express/app.js
@@ -3,17 +3,17 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("<h1>Welcome!</h1>");
-});
+app.use(express.static("public"));
 
-console.log(__dirname);
+//client side rendering. throwing index.html at the client and the client "paints" / renders it
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontpage/index.html");
+});
 
 app.get("/weather", (req, res) => {
     //res.send("<h1>Hello .... Want to know the weahter?</h1><h2>...hello??</h2>");
     res.sendFile(__dirname + "/public/weather.html");
 });
-
 
 app.listen(8080, () => {
     console.log("Server started at port: ", 8080);
